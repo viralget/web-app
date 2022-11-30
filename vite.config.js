304cfc1f7a77584dab60fbf184449cbf6af9e598
Yomi Omotoso <yomi@shopwired.uk>
2022-11-30 22:38:03 +0000

    website redesign

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
+            input: ['resources/js/app.jsx'], // Specify the entry points...
         }),
     ],
     resolve: {
