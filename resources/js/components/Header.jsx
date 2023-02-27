09f9944b9c510a6bff479c0c287bc1372c54e6cc
sheriffjimoh <sheriffjimoh88@gmail.com>
2023-02-27 12:44:56 -0800

    fix: mobile responsiveness

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -73,7 +73,7 @@ function MobileNavigation() {
   return (
     <Popover>
       <Popover.Button
-        className="relative z-10 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
+        className="relative z-50 flex h-8 w-8 items-center justify-center [&:not(:focus-visible)]:focus:outline-none"
         aria-label="Toggle Navigation"
       >
         {({ open }) => <MobileNavIcon open={open} />}
