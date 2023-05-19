1752e901cf4f11e2b63495595e095c8c76297395
Yomi Omotoso <yomi@shopwired.uk>
2023-05-19 18:14:37 +0100

    local

--- resources/js/Pages/TrackCampaigns/components/MetricsHeader.jsx
+++ resources/js/Pages/TrackCampaigns/components/MetricsHeader.jsx
@@ -3,11 +3,12 @@ import {
     EngagementRateWithBSvg
 } from '@/Utils/icons';
 import Card from "@/Pages/InfluencerProfile/card";
+import { ArrowPathRoundedSquareIcon } from "@heroicons/react/24/outline";
 const MetricsHeader = ({ metrics }) => {
 
-       const data = metrics;
-    
-       console.log("data:", data)
+    const data = metrics;
+
+    console.log("data:", data)
     const metricsData = [
         {
             title: 'Impressions',
@@ -39,20 +40,20 @@ const MetricsHeader = ({ metrics }) => {
         }
     ];
 
-    return(
+    return (
         <div className="mt-space-60 ">
-                <div className="flex    justify-end">
-                        <a href="#"    className='p-2  px-5  rounded-md bg-viralget-red  text-white font-lexend'> Refetch </a>
-                </div>
-                <div  className="grid mt-space-60  lg:grid-cols-4   grid-cols-1  gap-y-5 lg:gap-x-space-8"> 
-                    
-                    {
-                        metricsData?.length > 0 && metricsData?.map((item, index) => (
-                            <Card  item={item}  key={index} />
+            <div className="flex    justify-end">
+                <a href="#" className='p-2  px-5  rounded-md bg-viralget-red  text-white font-lexend flex items-center'> <ArrowPathRoundedSquareIcon className="w-4 h-4 mr-2" /> Refetch </a>
+            </div>
+            <div className="grid mt-space-60  lg:grid-cols-4   grid-cols-1  gap-y-5 lg:gap-x-space-8">
 
-                        ))
-                    }
-                </div>
+                {
+                    metricsData?.length > 0 && metricsData?.map((item, index) => (
+                        <Card item={item} key={index} />
+
+                    ))
+                }
+            </div>
         </div>
     )
 }
