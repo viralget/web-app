ca6ea655ac17272c78437d14011389eb3c279305
Yomi Omotoso <yomi@shopwired.uk>
2023-06-23 15:44:42 +0100

    ssl check

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
 
     <!-- Facebook Pixel Code -->
 <script>
