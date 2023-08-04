483424921f1dea903a213b4a6f7d50005f123b1b
Yomi Omotoso <yomi@shopwired.uk>
2023-08-04 13:30:13 +0100

    web ui cleanups

---
+++ public/build/assets/ValidationErrors.1dd71cf0.js
@@ -0,0 +1,1 @@
+import{R as e}from"./app.8d903797.js";import{E as o}from"./Error.238ce19a.js";function n({errors:r}){return Object.keys(r).length>0&&e.createElement(o,{message:"Sorry, an error occured.",errors:r})}export{n as V};
